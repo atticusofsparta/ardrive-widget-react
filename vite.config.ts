679bf7c3d8d250fad1b98bd755df49bb8236aa08
Atticus <atticusofsparta@protonmail.com>
2022-11-13 12:50:05 -0600

    setup base testing, lint, and formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [svgr(), react()],
+  // TODO: remove this when no longer deploying to GH pages
+  base: '/ardrive-widget-react/',
+});
